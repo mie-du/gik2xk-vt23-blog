b85a5dfd2a65a1b6a1441f0b9778f4973f8ece88
Mikaela Hedberg <mie@du.se>
2023-01-25 14:54:53 +0100

    L4

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { Typography, Box, AppBar, Toolbar } from '@mui/material';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './views/Home';
+import Posts from './views/Posts';
+import PostEdit from './views/PostEdit';
+import PostDetail from './views/PostDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blogg</h1>
+
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/">Hem</Link>
+            </Typography>
+            <Typography variant="h6" component="div">
+              <Link to="/posts">Alla inlägg</Link>
+            </Typography>
+            <Typography variant="h6" component="div">
+              <Link to="/postEdit">Skapa inlägg</Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/posts" element={<Posts></Posts>}></Route>
+          <Route path="/postEdit" element={<PostEdit></PostEdit>}></Route>
+          <Route path="/postDetail" element={<PostDetail></PostDetail>}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/views/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h2>Hem</h2>;
+}
+
+export default Home;
