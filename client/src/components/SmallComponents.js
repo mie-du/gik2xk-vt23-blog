6ce189f9c2f56892e94cde6d086b806ae19d43af
Mikaela Hedberg <mie@du.se>
2023-02-27 16:19:24 +0100

    L7

---
+++ client/src/components/SmallComponents.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { Paper } from '@mui/material';
+import { grey } from '@mui/material/colors';
+
+const CustomPaper = styled(Paper)({
+  padding: '1rem',
+  background: grey[200]
+});
+
+export { CustomPaper };
